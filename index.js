b9f04952ec1d415b9d0c3762c076018741149b1a
agustin <agustincatalano@gmail.com>
2021-05-20 17:38:23 -0300

    format file

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 //import express from 'express'
-const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
-const app = express()
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) =>{
-    console.log('access token ', accessToken)
-    console.log('refresh token ', refreshToken)
-    console.log ('profile ', profile)
-}))
+const app = express();
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token ", accessToken);
+      console.log("refresh token ", refreshToken);
+      console.log("profile ", profile);
+    }
+  )
+);
 
-app.get('/auth/google/callback', passport.authenticate('google'))
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.get("/auth/google/callback", passport.authenticate("google"));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
