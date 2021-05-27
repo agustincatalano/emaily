6026cc2efe1ae98cee7f2be1ff38fd7d51af27b5
agustin <agustincatalano@gmail.com>
2021-05-27 19:37:08 -0300

    hanlde existing user

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,10 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,7 +16,18 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: [profile.id] }).then((existingUser) => {
+        if (existingUser) {
+          //we already have a record with the given profile ID
+          done(null, existingUser)
+        } else {
+          //we don't have an user record with this ID
+          new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+        }
+      });
+
       console.log("access token ", accessToken);
       console.log("refresh token ", refreshToken);
       console.log("profile ", profile);
